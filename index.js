a6a147f2b92e9ce4497b45ab9472f5e3af1d9944
weeqeecombe50 <mynieagbona@hotmail.com>
2025-09-04 20:45:13 +0300

    Update welcome message to English

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('Willkommen zum Versionskontrollsystem!');
+    res.send('Welcome to the version control system!');
 });
 
 app.listen(PORT, () => {
-    console.log(`Server läuft auf Port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
